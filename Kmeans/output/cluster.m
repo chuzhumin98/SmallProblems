2b66f3a1facfbdd5896ef1c8143124f1a989ea42
chuzhumin98 <chuzhumin98@gmail.com>
2017-12-17 19:27:53 +0800

    update for graph

--- Kmeans/output/cluster.m
+++ Kmeans/output/cluster.m
@@ -4,7 +4,7 @@ size = 9;
 colors = ['k^';'r^';'g^';'b^']; % 分别是未聚类点，第1-3类的颜色 
 clusterColor = ['rs';'gs';'bs']; % 各聚类类别中心的点
 for (k = 1:1) 
-    for (i = 1:9)
+    for (i = k*12-11:k*12-3)
         type = info(i,3) + 2;
         if (k == 1) 
             type = 1;
@@ -16,13 +16,17 @@ for (k = 1:1)
         end
         hold on
     end
-    for (i = 10:12)
+    for (i = k*12-2:k*12)
         scatter(info(i,1),info(i,2),70,clusterColor(i - 9,:),'filled');
         hold on
     end
+    xlim([0.5,5.5])
+    ylim([0.5,5.5])
+    xlabel('X')
+    ylabel('Y')
+    titles = strcat('关于PPT上数据的聚类过程',num2str(k));
+    title(titles)
+    box on
+    file = strcat('img/sample1Process=',num2str(k),'.png');
+    saveas(gcf,file);
 end
-xlim([0.5,5.5])
-ylim([0.5,5.5])
-xlabel('X')
-ylabel('Y')
-title('关于PPT上数据的聚类过程图')
