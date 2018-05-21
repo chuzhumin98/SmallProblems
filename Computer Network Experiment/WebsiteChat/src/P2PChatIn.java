6f4b7795ffa5d0facdba0fd031107062e7f4d211
chuzhumin98 <chuzhumin98@gmail.com>
2018-05-21 21:41:15 +0800

    start to build chat framewordk

---
+++ Computer Network Experiment/WebsiteChat/src/P2PChatIn.java
@@ -0,0 +1,8 @@
+
+public class P2PChatIn extends Thread {
+	public UserInfo chatUser; //聊天对象的信息
+	
+	public void run() {
+		
+	}
+}
---
+++ Computer Network Experiment/WebsiteChat/src/UserInfo.java
@@ -0,0 +1,12 @@
+
+public class UserInfo {
+	public String IP; //用户的IP地址
+	public int port; //用户端口号
+	public String username; //用户名
+	
+	public UserInfo(String IP, int port, String name) {
+		this.IP = IP;
+		this.port = port;
+		this.username = name;
+	}
+}
